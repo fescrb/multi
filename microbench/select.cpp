b4312adaff11c77d60237cb3d6f271f0f4af6ca1
Fernando Escribano <fescrb@gmail.com>
2024-02-11 11:29:06 +0000

    Update benchmarks Add benchmarks for select

--- microbench/select.cpp
+++ microbench/select.cpp
@@ -12,18 +12,41 @@
 #include <tuple>
 #include <cmath>
 
-static void BM_multi_vector_tuple_select_iterate(benchmark::State& state) {
-    /*multi::vector<double, double, double> v;
+template<multi::details::tuple_like_element Container>
+static void BM_single_select_iterate(benchmark::State& state) {
+    Container v;
     v.reserve(state.range(0));
-    auto val = std::make_tuple(1.0, 1.0, 1.0);
+    // TODO should depend on Container's value_type
+    auto val = std::make_tuple(1.0, 1.0, 1.0, 1.0);
     for(std::size_t _ = 0; _ < state.range(0); ++_)
         v.push_back(val);
-    auto accum = std::make_tuple(0.0, 0.0, 0.0);
+    std::tuple_element_t<0, typename Container::value_type> accum = {};
     for(auto _ : state) {
         for(auto it : v | multi::select<0>)
-            std::get<0>(accum) += std::get<0>(it);
+            accum += std::get<0>(it);
     }
-    benchmark::DoNotOptimize(accum);*/
+    benchmark::DoNotOptimize(accum);
 }
 
-BENCHMARK(BM_multi_vector_tuple_select_iterate)->Range(8, std::pow(8,6));
+template<multi::details::tuple_like_element Container>
+static void BM_select_iterate(benchmark::State& state) {
+    Container v;
+    v.reserve(state.range(0));
+    // TODO should depend on Container's value_type
+    auto val = std::make_tuple(1.0, 1.0, 1.0, 1.0);
+    for(std::size_t _ = 0; _ < state.range(0); ++_)
+        v.push_back(val);
+    std::tuple<std::tuple_element_t<0, typename Container::value_type>, std::tuple_element_t<2, typename Container::value_type>>  accum = {};
+    for(auto _ : state) {
+        for(auto it : v | multi::select<0, 2>) {
+            std::get<0>(accum) += std::get<0>(it);
+            std::get<1>(accum) += std::get<1>(it);
+        }
+    }
+    benchmark::DoNotOptimize(accum);
+}
+
+BENCHMARK(BM_single_select_iterate<std::vector<std::tuple<double, double, double, double>>>)->Range(8, std::pow(8,6));
+BENCHMARK(BM_single_select_iterate<multi::vector<double, double, double, double>>)->Range(8, std::pow(8,6));
+BENCHMARK(BM_select_iterate<std::vector<std::tuple<double, double, double, double>>>)->Range(8, std::pow(8,6));
+BENCHMARK(BM_select_iterate<multi::vector<double, double, double, double>>)->Range(8, std::pow(8,6));
