aaa74ea8c6311783f8dae797e4d12e33f3e6b8c8
Fernando Escribano <fescrb@gmail.com>
2024-02-14 23:15:19 +0000

    Initial untested implementation of array & iterator

--- tests/array.cpp
+++ tests/array.cpp
@@ -6,3 +6,40 @@
 #include <gtest/gtest.h>
 
 #include <multi/array.hpp>
+
+using namespace multi;
+
+TEST(array, default_initialization) {
+    array<5, bool, int, double> array;
+
+    EXPECT_EQ(array.size(), 5);
+}
+
+TEST(array, array_initialization) {
+    array<5, bool, int, double> array = {{
+        {false, 0, 0.0},
+        {false, 1, 1.0},
+        {false, 2, 2.0},
+        {false, 3, 3.0},
+        {false, 4, 4.0},
+    }};
+
+    EXPECT_EQ(array.size(), 5);
+}
+
+TEST(array, copy_initialization) {
+    array<5, bool, int, double> array = {{
+        {false, 0, 0.0},
+        {false, 1, 1.0},
+        {false, 2, 2.0},
+        {false, 3, 3.0},
+        {false, 4, 4.0},
+    }};
+
+    auto b = array.cbegin();
+    auto e = array.cend();
+
+    // auto copy = array;
+
+    // EXPECT_EQ(array, copy);
+}
